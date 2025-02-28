b070b9df320830186ed45a73610d1076fd6b8bb3
sleeping <2523711871@qq.com>
2025-02-28 12:42:36 +0800

    test

---
+++ src/main/java/com/test/spring2025_2_28/Demo1/demo1.java
@@ -0,0 +1,7 @@
+package com.test.spring2025_2_28.Demo1;
+
+public class demo1 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/main/java/com/test/spring2025_2_28/Spring2025228Application.java
@@ -0,0 +1,13 @@
+package com.test.spring2025_2_28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2025228Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring2025228Application.class, args);
+    }
+
+}
